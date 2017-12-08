d75392fdcc382f3ad6e29ce233f427bd29eceb51
wuhaojie <w19961009@126.com>
2017-12-08 14:13:27 +0800

    init.

---
+++ installerlibrary/src/main/java/top/wuhaojie/installerlibrary/Downloader.java
@@ -0,0 +1,15 @@
+package top.wuhaojie.installerlibrary;
+
+/**
+ * Author: wuhaojie
+ * E-mail: w19961009@126.com
+ * Date: 2017/12/08 11:46
+ * Version: 1.0
+ */
+
+public class Downloader {
+
+    private Downloader() {
+    }
+
+}
